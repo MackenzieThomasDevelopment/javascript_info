6b9d5663d6bfa87f6377af9415a3c5e4e8b70713
Mackenzie Thomas <mackenzie.thomas54@gmail.com>
2018-05-10 20:55:37 -0500

    Finished up to but not including part 1.5

---
+++ 1.4/script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+let admin;
+let name;
+
+name = "John";
+
+admin = name;
+
+alert(admin);
+
+let ourPlanetName = "Earth";
+let currentUserName = "John";
+
+const BIRTHDAY = '18.04.1982';
+
+const age = 2*(BIRTHDAY);
